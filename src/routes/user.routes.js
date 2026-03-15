099bed985ee9ced09bc70e24aea020c9036e1566
Sumit patel <sp487330@gmail.com>
2026-03-15 13:00:45 +0530

    Add new route in the router folder

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -29,6 +29,20 @@ router.route("/logout").post(verifyJWT ,logoutUser)
 
 router.route("/refersh-token").post(refreshAccessToken)
 
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
 
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+// patch mai bas selected details hi update hoti hai lekin Post mai sari details hi update ho jati hai
+router.route("/update-accout").patch(verifyJWT, updateAccountDetails)
+
+// here we are using two middleware in which first we need to verify wheater the user is loggedin or not then futher multer middlewRE for uploading the file
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+// jab params se data lo tab thodi problem atti hai jaise ismai "/c/" add karna pada
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
